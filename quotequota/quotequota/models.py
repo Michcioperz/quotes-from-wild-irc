1773c61cdd46300f704d35a4885b434aa0809758
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2017-01-16 22:31:56 +0100

    stupid mistakes

--- quotequota/quotequota/models.py
+++ quotequota/quotequota/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 class Quote(models.Model):
     content = models.TextField()
     added_timestamp = models.DateTimeField(default=timezone.now)
+
+    def get_absolute_url(self):
+        return reverse("render-quote", args=(self.pk,))
