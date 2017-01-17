4137e9bf558e384a738046638958dd4a1c9c1227
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2017-01-17 17:20:42 +0100

    little helpers for admins

---
+++ quotequota/quotequota/templatetags/quotequota.py
@@ -0,0 +1,10 @@
+from django.core.cache import cache
+from django import template
+
+from quotequota.models import Quote
+
+register = template.Library()
+
+@register.simple_tag
+def quotecount():
+    return str(cache.get_or_set('quotes_count', Quote.models.count, timeout=300))
